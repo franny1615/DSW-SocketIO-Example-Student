174c9bb4180399a948cc96a10aecab7d68d45a56
francisco <franke1615@gmail.com>
2018-04-06 09:10:20 -0700

    Finished?

--- webapp.py
+++ webapp.py
@@ -10,6 +10,14 @@ socketio = SocketIO(app, async_mode=None)
 thread = None
 thread_lock = Lock() #we'll use this lock to prevent multiple clients from modifying thread at the same time
 
+def background_thread():
+	#this function does the counting
+	count = 0
+	while True:
+		socketio.sleep(5) #wait 5 seconds 
+		count += 1 #add 1 to count 
+		socketio.emit('my_response', count) #send count to all clients
+
 @socketio.on('connect') #run this when the connection starts
 def test_connect():
 	global thread
@@ -17,14 +25,6 @@ def test_connect():
 		if thread is None:
 			thread = socketio.start_background_task(target=background_thread)
 	emit('start', 'Connected')
-	
-def background_thread():
-	#this function does the counting
-	count = 0
-	while True:
-		socketio.sleep(5) #wait 5 seconds 
-		count += 1 #add 1 to count 
-		emit('my_response', count) #send count to all clients
 
 @app.route('/')
 def index():
